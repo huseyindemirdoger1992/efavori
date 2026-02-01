0226844b964d36a419b7030928f6dfe574bce8d6
husey <huseyindemirdoger1992@hotmail.com>
2026-02-01 19:23:51 +0300

    .

--- _PublisherAssistant/Form1.cs
+++ _PublisherAssistant/Form1.cs
@@ -22,12 +22,11 @@ namespace _PublisherAssistant
             label2.Text = $"Action Start At: {DateTime.Now}";
             TestButton.Enabled = false;
             string serverUrl = "ftp://94.199.202.149/";
-            string userName = "efavoriconnectionftp";
-            string password = "FeneriAslanYedi0!";
+            string userName = "efavoric";
+            string password = "u1jQ7.2PChO-1rSc!0";
 
             try
             {
-                // 1. WebRequest oluştururken metodu ListDirectory olarak değiştiriyoruz
                 FtpWebRequest request = (FtpWebRequest)WebRequest.Create(serverUrl);
                 request.Method = WebRequestMethods.Ftp.ListDirectory;
                 request.Credentials = new NetworkCredential(userName, password);

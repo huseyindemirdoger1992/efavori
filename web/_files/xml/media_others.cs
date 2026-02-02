980ffa67704b3bd8cd13fa1a1035b950905739d7
Hüseyin DEMİRDÖĞER <huseyindemirdoger1992@hotmail.com>
2026-02-02 14:43:00 +0300

    .

--- web/_files/xml/media_others.cs
+++ web/_files/xml/media_others.cs
@@ -1,7 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using razor._Shared.tr.Header;
+using System.Text;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace web._files.xml
 {
+    //🔁 Cache’li sitemap
+
+    //⚡ Redis destekli sitemap
+
+    //🌍 Multi-language sitemap
+
+    //🖼 Image sitemap
+
+    //🎥 Video sitemap
+
+    //🧠 Otomatik DB sayım + page hesaplama
+
+    // URL: https://efavori.com/_files/xml/media_others.xml?page=1
     [Route("_files/xml/media_others.xml")]
     public class media_others : Controller
     {
--- web/_files/xml/media_sountds.cs
+++ web/_files/xml/media_sountds.cs
@@ -2,6 +2,19 @@
 
 namespace web._files.xml
 {
+    //🔁 Cache’li sitemap
+
+    //⚡ Redis destekli sitemap
+
+    //🌍 Multi-language sitemap
+
+    //🖼 Image sitemap
+
+    //🎥 Video sitemap
+
+    //🧠 Otomatik DB sayım + page hesaplama
+
+    // URL: https://efavori.com/_files/xml/media_sountds.xml?page=1
     [Route("_files/xml/media_sountds.xml")]
     public class media_sountds : Controller
     {

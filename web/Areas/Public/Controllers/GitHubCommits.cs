00c655e3b079e4286e64accf0731b08bd834d31a
Hüseyin DEMİRDÖĞER <huseyindemirdoger1992@hotmail.com>
2026-02-05 16:05:35 +0300

    Pages klasörü oluşturuldu buna göre içerisinde controller yapısına göre dizinlemeler olcak bu sayade sayfalamalar daha kolay yapılabilinir olacak. Profil alanında da yazılımın versiyonlarına erişebilinecek yönlendirme linki verildi ve ilgili sayfa yapısı razor dan çekiliyor. Bu çekim işlemini yapabilmek için ilgili controller ve cshtml yapıları oluşturuldu. Not: Kullanıcı oturum açsın veya açmasın bu gelişim sürecine erişebiliyor.

---
+++ web/Areas/Public/Controllers/GitHubCommits.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace web.Areas.Public.Controllers
+{
+    [Area("Public")]
+    [Route("{culture}/Public/[controller]/[action]")]
+    public class GitHubCommits : Controller
+    {
+        public IActionResult GetGitHubCommits()
+        {
+            return View();
+        }
+    }
+}

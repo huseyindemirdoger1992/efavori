08fff4d895ac56f385605926a2b12ce365576891
Hüseyin DEMİRDÖĞER <huseyindemirdoger1992@hotmail.com>
2026-02-06 12:07:55 +0300

    publish

--- data/DeploymentLog.cs
+++ data/DeploymentLog.cs
@@ -1,18 +1,32 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Text;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace data
 {
+    [Table("DeploymentLog", Schema = "efavori")]
     public class DeploymentLog
     {
         [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
         public Guid Id { get; set; } = Guid.NewGuid();
-        public string? ProjectName { get; set; }
-        public string? CommitMessage { get; set; }
-        public string? DeployedBy { get; set; }
-        public DateTime? DeployDate { get; set; } = DateTime.Now;
-        public string? Status { get; set; }   
+
+        [Required]
+        [MaxLength(255)]
+        public string ProjectName { get; set; } = null!;
+
+        [Required]
+        public string CommitMessage { get; set; } = null!;
+
+        [Required]
+        [MaxLength(255)]
+        public string DeployedBy { get; set; } = null!;
+
+        [Required]
+        public DateTime DeployDate { get; set; } = DateTime.UtcNow;
+
+        [Required]
+        [MaxLength(50)]
+        public string Status { get; set; } = "null";
     }
 }
---
+++ data/Migrations/20260206090602_19.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace data.Migrations
+{
+    /// <inheritdoc />
+    public partial class _19 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "efavori");
+
+            migrationBuilder.RenameTable(
+                name: "DeploymentLog",
+                newName: "DeploymentLog",
+                newSchema: "efavori");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Status",
+                schema: "efavori",
+                table: "DeploymentLog",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ProjectName",
+                schema: "efavori",
+                table: "DeploymentLog",
+                type: "nvarchar(255)",
+                maxLength: 255,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DeployedBy",
+                schema: "efavori",
+                table: "DeploymentLog",
+                type: "nvarchar(255)",
+                maxLength: 255,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DeployDate",
+                schema: "efavori",
+                table: "DeploymentLog",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CommitMessage",
+                schema: "efavori",
+                table: "DeploymentLog",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameTable(
+                name: "DeploymentLog",
+                schema: "efavori",
+                newName: "DeploymentLog");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Status",
+                table: "DeploymentLog",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ProjectName",
+                table: "DeploymentLog",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(255)",
+                oldMaxLength: 255);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DeployedBy",
+                table: "DeploymentLog",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(255)",
+                oldMaxLength: 255);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DeployDate",
+                table: "DeploymentLog",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CommitMessage",
+                table: "DeploymentLog",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}

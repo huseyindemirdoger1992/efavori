8f09e32b991f18a7f54556bbe702a3dfee5dbd5c
husey <huseyindemirdoger1992@hotmail.com>
2026-02-11 03:31:12 +0300

    Görev kartı taşındıkca seçilen kartlara gelindiğinde tetiklenip bilgi emaili göndermek için alt yapı hazırlandı. Api servisinde metod oluşturulacak.Oluşturulan metod taşıma, güncelleme modalından güncelleme anında tetiklenecekve email gönderecek

---
+++ data/Migrations/20260210233743_28.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace data.Migrations
+{
+    /// <inheritdoc />
+    public partial class _28 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsCompleted",
+                table: "TaskStatus",
+                type: "bit",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsInEditing",
+                table: "TaskStatus",
+                type: "bit",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsInProgress",
+                table: "TaskStatus",
+                type: "bit",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsNew",
+                table: "TaskStatus",
+                type: "bit",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsToBeDone",
+                table: "TaskStatus",
+                type: "bit",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsCompleted",
+                table: "TaskStatus");
+
+            migrationBuilder.DropColumn(
+                name: "IsInEditing",
+                table: "TaskStatus");
+
+            migrationBuilder.DropColumn(
+                name: "IsInProgress",
+                table: "TaskStatus");
+
+            migrationBuilder.DropColumn(
+                name: "IsNew",
+                table: "TaskStatus");
+
+            migrationBuilder.DropColumn(
+                name: "IsToBeDone",
+                table: "TaskStatus");
+        }
+    }
+}
--- data/TaskStatus.cs
+++ data/TaskStatus.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
 using System.Text;
 
 namespace data
@@ -34,6 +35,15 @@ namespace data
 
         // "ToBeDone", "InProcess", "InEditing", "Completed"
         public string? Status { get; set; }
+
+        // Bildirim Email gönderilme durumu
+        public bool? IsNew { get; set; }
+        public bool? IsToBeDone { get; set; }
+        public bool? IsInProgress { get; set; }
+        public bool? IsInEditing { get; set; }
+        public bool? IsCompleted { get; set; }
+
+        // Tarihsel veriler
         public DateTime? DateToBeDone { get; set; } = DateTime.Now;
         public DateTime? DateInProcess { get; set; } = DateTime.Now;
         public DateTime? DateInEditing { get; set; } = DateTime.Now;

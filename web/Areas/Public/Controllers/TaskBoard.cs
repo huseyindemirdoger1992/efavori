6fedee8829f0789152205f2cd33dd6544c7f3392
husey <huseyindemirdoger1992@hotmail.com>
2026-03-08 03:48:07 +0300

    Görev çıktısı alınabiliyor

--- web/Areas/Public/Controllers/TaskBoard.cs
+++ web/Areas/Public/Controllers/TaskBoard.cs
@@ -9,12 +9,18 @@ namespace web.Areas.Public.Controllers
     [Route("{culture}/Public/[controller]/[action]")]
     [Route("{culture}/Public/[controller]/[action]/{Value?}")]
     [Route("{culture}/Public/[controller]/[action]/{Value}/{TaskFrameworkId}")]
+    [Route("{culture}/Public/[controller]/[action]/{TaskId:guid}")]
     public class TaskBoard : Controller
     {
         public IActionResult CentralSystemTaskBoard(string? Value, Guid? TaskFrameworkId)
         {
             ViewBag.Value = Value;
             ViewBag.TaskFrameworkId = TaskFrameworkId;
+            return View();
+        }
+        public IActionResult PrintTask(Guid? TaskId)
+        {
+            ViewBag.TaskId = TaskId;
             return View();
         }
     }

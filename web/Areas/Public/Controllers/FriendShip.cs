1ea60324573d5cc1a54792e58ebe3dba89ef3b3d
EXCALIBUR <EXCALIBUR@EXCALIBUR>
2026-03-20 03:52:21 +0300

    Arkadaşlık istekleri sayfasından arkadaşlık istekleri kabul edilip reddedilebiliniyor

---
+++ web/Areas/Public/Controllers/FriendShip.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace web.Areas.Public.Controllers
+{
+    [Area("Public")]
+    [Route("{culture}/Public/[controller]/[action]")]
+    public class FriendShip : Controller
+    {
+        public IActionResult Requests()
+        {
+            return View();
+        }
+    }
+}

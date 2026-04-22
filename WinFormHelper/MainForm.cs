35121c0d25ef358899a5c584c725c2b141ede327
huseyindemirdoger1992 <huseyindemirdoger1992@hotmail.com>
2026-04-22 10:05:06 +0300

    Masa üstü yardımcı yazıldı (For Windows .Net Core)

---
+++ WinFormHelper/MainForm.cs
@@ -0,0 +1,16 @@
+using System.Net.NetworkInformation;
+
+namespace WinFormHelper
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private async void MainForm_Load(object sender, EventArgs e)
+        {
+        }
+    }
+}

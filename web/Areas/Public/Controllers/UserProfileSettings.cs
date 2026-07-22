4242c60edde568f4c42da5fb596d28eacb15ab84
huseyindemirdoger1992 <huseyindemirdoger1992@hotmail.com>
2026-07-22 17:50:28 +0300

    .

--- web/Areas/Public/Controllers/UserProfileSettings.cs
+++ web/Areas/Public/Controllers/UserProfileSettings.cs
@@ -3,13 +3,7 @@
 namespace web.Areas.Public.Controllers
 {
     [Area("Public")]
-    [Route("")]
-    [Route("{TaskFrameworkId:guid}")]
-    [Route("{Value?}/{TaskFrameworkId?}")]
     [Route("{culture}/Public/[controller]/[action]")]
-    [Route("{culture}/Public/[controller]/[action]/{Value?}")]
-    [Route("{culture}/Public/[controller]/[action]/{Value}/{TaskFrameworkId}")]
-    [Route("{culture}/Public/[controller]/[action]/{TaskId:guid}")]
     public class UserProfileSettings : Controller
     {
 
